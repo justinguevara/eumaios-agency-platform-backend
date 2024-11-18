276fcba9a17944f1e1617d010801b8e64d1cb969
Justin Guevara <k@guevarajust.in>
2024-11-18 11:18:55 -0500

    fix authorization

--- routes/api.php
+++ routes/api.php
@@ -13,46 +13,48 @@ use App\Http\Controllers\NetworksController;
 use App\Http\Controllers\PublishersController;
 use App\Http\Controllers\CampaignsController;
 
-Route::resources([
-    '1/campaigns' => CampaignsController::class,
-]);
+Route::middleware(['auth'])->group(function () {
+    Route::apiResources([
+        '1/campaigns' => CampaignsController::class,
+    ]);
+});
 
-Route::controller(NetworksController::class)
-    ->group(
-        function () {
-            Route::get('1/networks', 'index');
-            Route::post('1/networks', 'store');
-            Route::get('1/networks/{network}', 'show');
-            Route::put('1/networks/{network}', 'update');
-            // destroy() not implemented - use PUT route
-        }
-    );
+Route::middleware(['auth'])
+    ->controller(NetworksController::class)
+    ->group(function () {
+        Route::get('1/publishers', 'index');
+        Route::post('1/publishers', 'store');
+        Route::get('1/publishers/{publisher}', 'show');
+        Route::put('1/publishers/{publisher}', 'update');
+        Route::delete('1/publishers/{publisher}', 'destroy');
+});
 
-Route::controller(PublishersController::class)
-    ->group(
-        function () {
-            Route::get('1/publishers', 'index');
-            Route::post('1/publishers', 'store');
-            Route::get('1/publishers/{publisher}', 'show');
-            Route::put('1/publishers/{publisher}', 'update');
-            Route::delete('1/publishers/{publisher}', 'destroy');
-        }
-    );
-
-Route::post('1/register-user', [RegisteredUserController::class, 'store'])
-    ->middleware([App\Http\Middleware\GlobalAccessTokenMiddleware::class])
-    ->name('register');
+Route::middleware(['auth'])
+    ->controller(PublishersController::class)
+    ->group(function () {
+        Route::get('1/networks', 'index');
+        Route::post('1/networks', 'store');
+        Route::get('1/networks/{network}', 'show');
+        Route::put('1/networks/{network}', 'update');
+        // destroy() not implemented - use PUT route
+});
 
 Route::post('1/login', [AuthenticatedSessionController::class, 'store'])
     // ->middleware('guest') // TODO review
     ->name('login');
 
-Route::post('1/logout', [AuthenticatedSessionController::class, 'destroy'])
-    ->middleware('auth')
-    ->name('logout');
+Route::middleware(['auth'])->group(function () {
+    Route::post('1/register-user', [RegisteredUserController::class, 'store'])
+        ->middleware(['auth', App\Http\Middleware\GlobalAccessTokenMiddleware::class])
+        ->name('register');
 
-Route::get('1/user', function (Request $request) {
-    return $request->user();
+    Route::post('1/logout', [AuthenticatedSessionController::class, 'destroy'])
+        ->middleware('auth')
+        ->name('logout');
+
+    Route::get('1/user', function (Request $request) {
+        return $request->user();
+    });
 });
 
 // TODO review/implement
