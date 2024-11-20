a2bbfc9b62b81e1503a6244e53726fad7fb3606f
Justin Guevara <k@guevarajust.in>
2024-11-20 14:18:25 -0500

    fix tests

--- app/Models/Publisher.php
+++ app/Models/Publisher.php
@@ -7,9 +7,11 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Publisher extends Model
 {
+    use HasFactory;
     use SoftDeletes;
 
     private ?string $name;
--- tests/Unit/PublishersControllerTest.php
+++ tests/Unit/PublishersControllerTest.php
@@ -2,29 +2,22 @@
 
 namespace Tests\Unit;
 
-use Tests\TestCase;
-use App\Http\Controllers\PublishersController;
-use App\Models\Publisher;
-use Illuminate\Http\JsonResponse;
-use Mockery;
+use \Tests\TestCase;
+use \App\Http\Controllers\PublishersController;
+use \App\Models\Publisher;
+use \Illuminate\Http\JsonResponse;
 use \Illuminate\Http\Response;
 
 class PublishersControllerTest extends TestCase
 {
     public function testPublishersControllerIndex()
     {
-        $publishers = [
-            ['id' => 1, 'name' => 'P1'],
-            ['id' => 2, 'name' => 'P2'],
-        ];
-        $publishers_mock = Mockery::mock('alias:' . Publisher::class);
-        $publishers_mock->shouldReceive('all')
-            ->once()
-            ->andReturn(collect($publishers));
+        Publisher::factory()->create(['name' => 'P1']);
+        Publisher::factory()->create(['name' => 'P2']);
+        $publishers = Publisher::all()->toArray();
 
         $controller = new PublishersController();
         $response = $controller->index();
-
         $this->assertInstanceOf(JsonResponse::class, $response);
         $this->assertEquals(Response::HTTP_OK, $response->getStatusCode());
         $this->assertInstanceOf(JsonResponse::class, $response);
